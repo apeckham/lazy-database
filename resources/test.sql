146b41a7f5d498221e2787944dd832ea470182ed
Aaron Peckham <apeckham@gmail.com>
2019-04-22 22:32:49 -0700

    generate more data

--- resources/test.sql
+++ resources/test.sql
@@ -1,10 +1,10 @@
 -- :name now :? :1
 select now()
 
--- :name bigdata :? :*
+-- :name bigdata :? :raw
 select
   left(md5(i::text), 10),
   md5(random()::text),
   md5(random()::text),
   left(md5(random()::text), 4)
-from generate_series(1, 1000000) s(i)
+from generate_series(1, 100000000) s(i)
